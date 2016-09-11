ce0bbbfe178f0692aaeab3e33c52c8635d7fcd66
unknown <albertzhu.7@gmail.com>
2016-09-11 16:39:40 -0300

    update

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 'use strict'
 
-console.log('Hello,World!');
+let randNum = (min,max) => {
+  return Math.random() * (max - min + 1) + min;
+}
+
+
+let min = 100;
+let max = 1000;
+let ans;
+
+let num = randNum(min, max).toFixed(0);
+
+console.log('I\'m thinking of a number it is: '+num);
