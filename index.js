0441350aa9722414a4bb41e9ee378492ed04e118
unknown <albertzhu.7@gmail.com>
2016-09-25 14:32:51 -0300

    add jshintrc   update index.js to loop through guesses and validate

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 'use strict';
 
+let num;
+let min = 100;
+let max = 200;
+let tries = 0;
+let maxTries = 5;
+
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
 let randNum = (min,max) => {
   return Math.random() * (max - min + 1) + min;
-}
+};
 
-
-let min = 100;
-let max = 102;
-
-
-let num = randNum(min, max).toFixed(0);
-
-
-console.log('I\'m thinking of a number from '+min+' to '+max+'\n');
-rl.question('What is your guess: ', (answer) => {
-
-  if(answer === num){
-    console.log('WHAT THE HECK!!! YOU GUESSED THE NUMBER! THERE WAS A 0.001 CHANCE YOU COULD HAVE DONE THAT');
+let getAnswer = () => {
+  if(tries <= maxTries){
+    tries++;
+    rl.question('What is your guess: ', (answer) => {
+      if(answer === num){
+        rl.close();
+        console.log('WHAT THE HECK!!! YOU GUESSED THE NUMBER🎉🎉🎉🎉🎉🎉!!! THERE WAS A 0.00000000000000000000000001 CHANCE YOU COULD HAVE DONE THAT');
+        process.exit(0);
+      }else{
+        console.log('SORRY YOU SUCK😐😐😐😐😐😐!!!!!!');
+        getAnswer();
+      }
+  });
   }else{
-    console.log('SORRY YOU SUCK!!!!!!');
+    console.log('fail: too many guesses');
+    process.exit(1);
   }
-  console.log('You guesses %s, I was thinking of %s', answer, num);
-  rl.close();
-});
+};
+
+num = randNum(min, max).toFixed(0);
+console.log('I\'m thinking of a number from '+min+' to '+max+'\n');
+getAnswer();
